f784d85736177b4f4e7c99fd55da3e1161d98ba1
Ellen Paixao Silva <ellen.silva@spassu.com.br>
2021-10-17 23:50:16 -0300

    nova interface

--- mBook/Properties/Resources.Designer.cs
+++ mBook/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace mBook.Properties {
     // através de uma ferramenta como ResGen ou Visual Studio.
     // Para adicionar ou remover um associado, edite o arquivo .ResX e execute ResGen novamente
     // com a opção /str, ou recrie o projeto do VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -63,9 +63,9 @@ namespace mBook.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap capa_ET {
+        internal static System.Drawing.Bitmap cover {
             get {
-                object obj = ResourceManager.GetObject("capa ET", resourceCulture);
+                object obj = ResourceManager.GetObject("cover", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
